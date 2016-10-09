a1305dc70c220a627e54c56f0b643ba4cd5a4137
Sonal Bairwa <bairwasonal20@gmail.com>
2016-10-09 14:50:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/',function(req,res){
 });
 
 app.get('article-one',function(req,res){
-    res.send('Article two is requested and will be served here');
+    res.send('Article  is requested and will be served here');
 });
 
 app.get('article-two',function(req,res){
