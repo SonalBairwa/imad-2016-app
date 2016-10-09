01ce9abf2c5e0a4a33326327345488e67e901277
Sonal Bairwa <bairwasonal20@gmail.com>
2016-10-09 14:47:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,19 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
+app.get('/',function(req,res){
+    res.send('Article one is requested and will be served here');
+});
+
+app.get('article-one',function(req,res){
+    res.send('Article one is requested and will be served here');
+});
+
+app.get('article-two',function(req,res){
+    res.send('Article one is requested and will be served here');
+});
+
+app.get('article-three', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
