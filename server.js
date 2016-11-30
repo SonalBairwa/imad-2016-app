a0357e31359f4844692e33873e5bdf94ccd9796b
Sonal Bairwa <bairwasonal20@gmail.com>
2016-11-30 21:06:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/animation.html',function(req,res){
+app.get('/sonal/cs/animation.html',function(req,res){
    res.sendFile(path.join(__dirname, 'ui/sonal/css', 'animation.html'));
 });
 
