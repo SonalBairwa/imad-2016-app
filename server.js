182d522e4d2db4ebd1676a5065ed99eb63cb7d70
Sonal Bairwa <bairwasonal20@gmail.com>
2016-12-02 06:18:13 -1000

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname, 'ui', '/index.html'));
 });
 
 app.get('/ui',function(req,res){
-   res.sendFile(path.join(__dirname, + '/animation.css'));
+   res.sendFile(path.join(__dirname,'ui', '/animation.css'));
 });
 
 
